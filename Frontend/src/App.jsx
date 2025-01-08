d56c965db33298ac3fcaa3ac9e5aafadd2b087a5
Daotama <daobrendan7@gmail.com>
2025-01-08 12:28:52 -0500

    Link Routes for Sign Up, Sign In, and Profile

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./Components/Layout"
 import Home from "./Pages/Home"
+import Signup from "./Pages/SignUp"
+import Signin from "./Pages/SignIn"
+import Profile from "./Pages/Profile"
 function App() {
 
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="" element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
