b3f57107dc4b67eebbfc08fd64a55af799adcef6
Daotama <daobrendan7@gmail.com>
2025-02-24 17:11:30 -0500

    Restoring More Changes

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import FollowSuggestionCard from './FollowSuggestionCard'
+import Tag from './Tag'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className=' text-white p-4 gap-6 flex flex-col justify-around h-screen w-[330px] fixed top-0 right-0 bg-[#0A0B10]'>
+        <div className='relative flex gap-4 flex-col top-[100px]'>
+          <FollowSuggestionCard fullName={"Full Name"} username={"username"} imgSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpuYdLEzBvwemix8pwsncUkLLOQqnByncadg&s"} />
+          <FollowSuggestionCard fullName={"Full Name"} username={"username"} imgSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpuYdLEzBvwemix8pwsncUkLLOQqnByncadg&s"} />
+          <FollowSuggestionCard fullName={"Full Name"} username={"username"} imgSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpuYdLEzBvwemix8pwsncUkLLOQqnByncadg&s"} />
+          <FollowSuggestionCard fullName={"Full Name"} username={"username"} imgSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpuYdLEzBvwemix8pwsncUkLLOQqnByncadg&s"} />
+        </div>
+  
+        <div className="tags grid grid-cols-2 gap-2">
+          <Tag classnames={"grid col-span-2"} text={"About Us"} />
+          <Tag text={"Contact Us"} />
+          <Tag text={"Policy"} />
+          <Tag classnames={"grid col-span-2 w-fit"} text={"© 2025 Networking Tool. All rights reserved."} />
+        </div>
+    </footer>
   )
 }
 
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,54 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { FaLinkedin, FaInstagram, FaYoutube   } from "react-icons/fa6";
+import SearchInput from '../Components/SearchInput';
+import FilterQuestionTab from '../Components/FilterQuestionTab';
+import articles from '../assets/FakeData.js'
+import ExpandableCard from '../Components/ExpandableCard.jsx';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+  const auth = useSelector((state) => state.auth)
+  console.log("auth from the home page ", auth)
+  const [searchQuery,setSearchQuery] = useState("")
+  const [filterQuery, setFilterQuery] = useState("")
+
+  const handleSearch = (userquery) =>{
+    setSearchQuery(userquery)
+    console.log(userquery)
+  }
+
+  const handleFilterChosen = (userquery) =>{
+    setFilterQuery(userquery)
+    console.log(userquery)
+  }
+
   return (
-    <section className='bg-[#25438B] h-fit w-full p-6'>
-      <header className='bg-white rounded-md p-6 flex flex-col gap-6'>
-        <h1 className='font-extrabold text-3xl text-center'>A GET TOGETHER FOR <br /> <span className='text-[#21c7e7]'>LIKE-MINDED PEOPLE</span></h1>
-        <div className='flex px-10 gap-10'>
-          <div>
-            <p className='text-2xl font-bold'>Our executive network provides a trusted platform for senior professionals to collaborate, share insights, and build strategic partnerships that fuel growth and innovation.</p>
-          </div>
-          <hr className='w-[40px] h-[200px] bg-gray-500' />
-          <div>
-            <p className='text-2xl font-bold'>Join a global network of industry leaders to exchange ideas, access exclusive resources, and discover opportunities that accelerate personal and organizational success.</p>
-          </div>
-          <hr className='w-[40px] h-[200px] bg-gray-500' />
-          <div>
-            <p className='text-2xl font-bold'>Elevate your influence and impact by connecting with top executives in a dynamic environment designed for knowledge-sharing, mentorship, and business advancement.</p>
-          </div>
-        </div>
+    <div className='min-h-screen text-white py-8 gap-8 flex flex-col px-8 max-h-fit w-[calc(100%-330px)] bg-gradient-to-r from-[#0A0B10] to-black'>
+      <header className='flex justify-between'>
+        <h1 className='text-[30px] font-bold'>All Questions</h1>
+        <Link className='bg-custom-gradient p-4 rounded-lg font-semibold'>Ask a Question</Link>
       </header>
+      <SearchInput onSearchChange={handleSearch} placeholderText={"Search a Question here"} classNames={"w-full"} />
+      <FilterQuestionTab onChosenFilter={handleFilterChosen} />
+      {articles.map((item, index) =>{
+        return (<ExpandableCard id={index} author={item.author} tags={item.tags} votes={item.likes} answers={item.comments.length} views={item.views} tittle={item.title}/>)
 
-      <section className='p-6 mt-12 rounded-md bg-[#8da7cf] w-full h-fit'>
-        <h1 className=' text-center font-extrabold text-3xl'>Visionary Hive</h1>
-        <p className='w-fit mt-[12px] m-auto'>Your ideas <br /> revolutionize the world</p>
-        <div className='flex justify-around mt-[12px]'>
-          <Link className='bg-[#b1b2b5] p-5 rounded-lg' to={""}>Start Networking</Link>
-          <Link className='bg-[#b1b2b5] p-5 rounded-lg' to={""}>Sign Up</Link>
-        </div>
-      </section>
-
-      <section className='w-full bg-white h-fit p-6 rounded-md mt-12'>
-        <h1 className='text-center font-extrabold text-3xl'>Contact Us</h1>
-        <div>
-          <div className='flex items-center gap-6'>
-            <Link><FaLinkedin className='text-6xl text-blue-600' /></Link>
-            <p className='text-2xl'>PlaceHolder</p>
-          </div>
-          <div className='flex items-center gap-6'>
-            <Link><FaInstagram className='text-6xl text-red-500'/></Link>
-            <p className='text-2xl'>PlaceHolder</p>
-          </div>
-          <div className='flex items-center gap-6'>
-            <Link><FaYoutube className='text-6xl text-red-700' /></Link>
-            <p className='text-2xl'>PlaceHolder</p>
-          </div>
-        </div>
-      </section>
-    </section>
+      })}
+    </div>
   )
 }
 
