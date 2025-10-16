fc4272dbad0a83818f6cf448c5587d3637ddeba3
Dao <73803690+daotame@users.noreply.github.com>
2025-10-15 20:54:40 -0400

    Small Changes

--- Frontend/src/Pages/ForgotPassword.jsx
+++ Frontend/src/Pages/ForgotPassword.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import emailjs from 'emailjs-com'
-import validator from "validator"
 
 const ForgotPassword = () => {
 
