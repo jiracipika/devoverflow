0df94094f6bde0848d6fef9669a15bd7ef9ae14a
Dao <73803690+daotame@users.noreply.github.com>
2025-10-15 20:51:45 -0400

    Emailjs for ForgetPassword

--- Frontend/src/Pages/ForgotPassword.jsx
+++ Frontend/src/Pages/ForgotPassword.jsx
@@ -1,58 +1,44 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
-import axios from 'axios'
+import emailjs from 'emailjs-com'
 import validator from "validator"
 
-
 const ForgotPassword = () => {
-    const [email, setEmail] = useState('');
+
+    const [formData, setFormData] = useState({
+        email: "",
+    })
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
+        console.log(e.target)
         e.preventDefault();
 
-        if (email === "") {
+        if (formData.email === "") {
             toast.error(`Please Enter Email`, {
                 autoClose: 3000
             });
             return
         }
-        if (!validator.isEmail(email)) {
-            toast.error(`Invalid Email`, {
+
+        else {
+            emailjs
+            .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY)
+            .then((result) => {
+                alert(`Email Sent! Please Check Your Inbox`);
+                setFormData({ email: "" })
+                navigate(`/thankyoupassword`);
+            }).catch(() => toast.error("Oops! Something went wrong. Please try again.", {
                 autoClose: 3000
-            });
-            return;
+            }))
         }
-
-        else{
-            console.log("Sending Email....")
-            //Send Email
-            // https://jsonplaceholder.typicode.com/posts
-            // http://localhost:5173/api/ForgotPassword
-            const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-                email: email,
-            });
-            // If the email was sent successfully, navigate
-            if (response.data.success === false) {
-                toast.error(response.data.message, {
-                    autoClose: 5000,
-                });
-            } else {
-                alert("Email Sent");
-                navigate(`/thankyoupassword?email=${encodeURIComponent(email)}`);
-
-            }
-            
-        }
-        
     }
-    
     return (
         <section className='w-full h-fit bg-[#0F1117]'>
             <ToastContainer />
             <section className='sign-in min-h-screen max-h-fit flex justify-center w-full p-6 flex-col items-center'>
-                <form className='bg-[#151821] w-[90%] h-4/5 flex flex-col gap-3 max-w-[450px] mt-5 pt-[50px] pb-[70px] px-[60px] rounded-[20px] left-2/4 top-2/4'>
+                <form onSubmit={handleSubmit} className='bg-[#151821] w-[90%] h-4/5 flex flex-col gap-3 max-w-[450px] mt-5 pt-[50px] pb-[70px] px-[60px] rounded-[20px] left-2/4 top-2/4' id="forgot-password-form">
                     <div className='flex justify-between items-center'>
                         <div>
                             <h1 className='text-white'>logo</h1>
@@ -60,13 +46,20 @@ const ForgotPassword = () => {
                             <p className='text-sm text-[#858EAD]'>Please Enter the Email you would like to send instructions to.</p>
                         </div>
                     </div>
-                    <div className='input-group'>
+                    <div className=''>
                         <div className='flex items-center mx-0 my-[15px]'>
-                            <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" className='bg-[#212734] text-[#858EAD] rounded-md w-full px-[15px] py-[18px] border-0 outline-none placeholder-#858EAD'/>
+                            <input 
+                                type="email" 
+                                name="email"
+                                value={formData.email} 
+                                onChange={(e) => setFormData({...formData, email: e.target.value})} 
+                                placeholder="example@gmail.com" 
+                                className='bg-[#212734] text-[#858EAD] rounded-md w-full px-[15px] py-[18px] border-0 outline-none placeholder-#858EAD'
+                            />
                         </div>
                     </div>
                     <div className='w-full '>
-                    <button onClick={handleSubmit} className='bg-gradient-to-r from-[#FF7000] to-[#E2995F] text-sm basis-[48%] h-10 w-full rounded-md text-white font-medium border-0 outline-none' type="button">Continue</button>
+                    <button className='bg-gradient-to-r from-[#FF7000] to-[#E2995F] text-sm basis-[48%] h-10 w-full rounded-md text-white font-medium border-0 outline-none' type="submit" >Continue</button>
                     </div>
                     <div className='flex gap-3 flex-col justify-center mx-0 my-[15px] rounded-[3px]'>
                          <Link to={"/signin"} className='self-center text-[#FF7000]'>Back to Sign In</Link>
